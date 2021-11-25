f29de3685e8b1951837fdabfa6fd1daa36c1d106
avpp <avijit.biswas@lowes.com>
2021-11-25 19:27:41 +0530

    Testing Jenkins-WebHook-Functionality or Auto-Build Trigger

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -18,7 +18,7 @@ import org.springframework.web.client.RestTemplate;
 public class NumericController {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
-	private static final String baseURL = "http://node-service:5000/plusone";
+	private static final String baseURL = "http://localhost:5000/plusone";
 	
 	RestTemplate restTemplate = new RestTemplate();
 	
@@ -48,6 +48,7 @@ public class NumericController {
 			logger.info("Value Received in Request - " + value);
 			logger.info("Node Service Response - " + response);
 			return Integer.parseInt(response);
+
 		}
 	}
 
