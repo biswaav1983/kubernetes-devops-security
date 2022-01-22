6bc9762f2c9d469c0b53eac84a611bb728dc4d1b
biswaav1983 <biswaav1983@github.com>
2022-01-22 19:29:14 +0000

    Added CIS-Scan

---
+++ cis-kubelet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#cis-kubelet.sh
+
+total_fail=$(/usr/local/bin/kube-bench --config-dir /usr/local/bin/cfg --config /usr/local/bin/cfg/config.yaml  run --targets node  --version 1.15 --check 4.2.1,4.2.2 --json | jq '.Totals | .total_fail')
+
+if [[ "$total_fail" -ne 0 ]];
+        then
+                echo "CIS Benchmark Failed Kubelet while testing for 4.2.1, 4.2.2"
+                exit 1;
+        else
+                echo "CIS Benchmark Passed Kubelet for 4.2.1, 4.2.2"
+fi;
