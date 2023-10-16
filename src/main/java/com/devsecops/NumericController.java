0971ca1f25dd60a39672bd2357b58bf278bbaa03
root <root@k8master.us-east4-c.c.gcp-clqa-security-tvm.internal>
2023-10-16 10:09:26 +0000

    Added Secret Files

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -13,8 +13,10 @@ import org.springframework.web.client.RestTemplate;
 public class NumericController {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
-	private static final String baseURL = "http://node-service:5000/plusone";
-	//private static final String baseURL = "http://localhost:5000/plusone";
+	//private static final String baseURL = "http://node-service:5000/plusone";
+	//private static final String baseURL = "http://172.17.0.2:5000/plusone";
+	
+	private static final String baseURL = "http://10.0.3.2:8787/plusone";
 	
 	RestTemplate restTemplate = new RestTemplate();
 	
